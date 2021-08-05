eda32197edbaad873b904184310a6652fb777a85
hgtlzyc <hgtlzycwk@gmail.com>
2021-08-05 08:49:43 -0400

    update display logic

--- AllVehicleModelsAPI/AllVehicleModelsAPI/Controllers/ViewControllers/VehicleModelsViewController.swift
+++ AllVehicleModelsAPI/AllVehicleModelsAPI/Controllers/ViewControllers/VehicleModelsViewController.swift
@@ -29,6 +29,7 @@ class VehicleModelsViewController: UIViewController {
     
     var models: [(String,Bool)] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +39,7 @@ class VehicleModelsViewController: UIViewController {
         answerInputTF.delegate = self
         
         setupInitialViews()
-        loadNewViewModel()
+        loadNewViewModel(reloadTable: false)
         
         //Dismiss kb
         NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -46,6 +47,7 @@ class VehicleModelsViewController: UIViewController {
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         view.addGestureRecognizer(tap)
+        
         view.isUserInteractionEnabled = true
         
     }//End Of ViewDidLoad
@@ -55,8 +57,6 @@ class VehicleModelsViewController: UIViewController {
     
     func setupInitialViews() {
         tableView.layer.cornerRadius = 10
-        questionLabel.text = ""
-        statusLabel.text = ""
         checkUserAnswerButton.layer.cornerRadius = 15
         showAllButton.layer.cornerRadius = 15
         nextQuestionButton.layer.cornerRadius = 15
@@ -77,7 +77,7 @@ class VehicleModelsViewController: UIViewController {
     
     @IBAction func nextButtonTapped(_ sender: Any) {
         resetTableViewEmpty()
-        loadNewViewModel()
+        loadNewViewModel(reloadTable: true)
     }//
     
     
@@ -110,8 +110,10 @@ extension VehicleModelsViewController: UITableViewDataSource, UITableViewDelegat
         
         let backgroundColor = models[indexPath.row].1 ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         
+       
         cell.contentView.backgroundColor = backgroundColor
-                
+        
+        
         return cell
     }
         
@@ -130,9 +132,10 @@ extension VehicleModelsViewController {
     }
     
     // MARK: - Load New ViewModel Related
-    func loadNewViewModel(){
+    func loadNewViewModel(reloadTable: Bool){
         viewModel.resetForNextQuestion()
         VehicleModelsControler.cancelAllPendingTasks()
+        setViewsForCurrentlyLoading(reloadTable: reloadTable)
         
         let nextID = generateRandomIDInRange()
         VehicleModelsControler.fetchModelsFor(nextID) { [weak self] result in
@@ -143,6 +146,7 @@ extension VehicleModelsViewController {
                     let targetAnswers = self?.generateTargetAnswersBasedOn(models)
                     self?.viewModel.setCorrectAnswers(targetAnswers ?? [""])
                     self?.pullDataFromViewModel(.userCorrectAnswers)
+                    self?.setViewsForFinishedLoading()
                 }
                 
             case .failure(let err):
@@ -153,15 +157,6 @@ extension VehicleModelsViewController {
         
     }//End Of function
     
-    //TextField Related
-    func putTextFieldTextInViewModel(_ text: String) {
-        let userAnswerText = generateFilteredString(text)
-        viewModel.newUserAnswered(userAnswerText)
-        pullDataFromViewModel(.userCorrectAnswers)
-        
-        answerInputTF.text = ""
-        answerInputTF.resignFirstResponder()
-    }
     
     // MARK: - String related
     func generateTargetAnswersBasedOn(_ models: [VehicleModel]) -> [String]{
@@ -180,9 +175,14 @@ extension VehicleModelsViewController {
     
     
     // MARK: - VM helpers
-    func resetTableViewEmpty(){
-        models = []
-        tableView.reloadData()
+    
+    func putTextFieldTextInViewModel(_ text: String) {
+        let userAnswerText = generateFilteredString(text)
+        viewModel.newUserAnswered(userAnswerText)
+        pullDataFromViewModel(.userCorrectAnswers)
+        
+        answerInputTF.text = ""
+        answerInputTF.resignFirstResponder()
     }
     
     func pullDataFromViewModel(_ type: ModelsDisplayType) {
@@ -197,7 +197,33 @@ extension VehicleModelsViewController {
         tableView.reloadData()
     }
     
-    //KeyBoard Related
+    // MARK: - Disply status related
+    
+    func setViewsForCurrentlyLoading(reloadTable: Bool){
+        questionLabel.text = "Loading Data ..."
+        statusLabel.text = "Loading data from NHTSA"
+        SetAllButtonEnableStatus(false)
+        
+        guard reloadTable else { return }
+        resetTableViewEmpty()
+    }
+    
+    func setViewsForFinishedLoading(){
+        SetAllButtonEnableStatus(true)
+    }
+    
+    func resetTableViewEmpty(){
+        models = []
+        tableView.reloadData()
+    }
+    
+    func SetAllButtonEnableStatus(_ shouldEnable: Bool){
+        [showAllButton, nextQuestionButton, checkUserAnswerButton].forEach { btn in
+            btn?.isEnabled = shouldEnable
+        }
+    }
+    
+    //Mark: -KeyBoard Related
     
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
         answerInputTF.resignFirstResponder()
@@ -214,4 +240,6 @@ extension VehicleModelsViewController {
         guard self.view.frame.origin.y != 0 else { return }
         self.view.frame.origin.y = 0
     }
-}
+    
+    
+}//End Of Extension
